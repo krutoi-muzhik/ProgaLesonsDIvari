625c3b2275899ebabe09000cb770eb6cc285a7fe
krutoi-muzhik <71013428+krutoi-muzhik@users.noreply.github.com>
2020-10-30 14:18:20 +0300

    Update ProgrammCheck.cpp

--- ProgrammCheck.cpp
+++ ProgrammCheck.cpp
@@ -1,21 +1,12 @@
-#define CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-
 int Verification(int N, struct client* bank, struct client* bank_new);
 
 int Check(struct balance* total_balance);
 
-int main() {
-	int i1 = 0;
-	int i2 = 0;
-	i1 = Verification(N, client* bank, client* bank_new);
-	i2 = Check(balance* total_balance);
-}
+
 
 int Verification(int N, struct client bank*, struct client* bank_new) {
-	int i = 0;                                         // счетчик
-	int BreakOrNot = 1;                                // если 1 - цикл идет, 0 - цикл завершается
+	int i = 0;                                         
+	int BreakOrNot = 1;                                
 	for (i = 0; (i < N) && (BreakOrNot); i ++) {
 		if (bank[i].identification_number != bank_new[i].identification_number) BreakOrNot = 0;
 		if (bank[i].account != bank_new[i].account) BreakOrNot = 0;
